c66cef228f17851e8a12f4876ea4045aafd4f14f
Aleksei Poluhin <Poluhin1979@outlook.com>
2022-06-30 17:31:56 +0300

    добавил в проек materialize и сделал с его помощью меню

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems);
+
+	var gallery = document.querySelectorAll('.materialboxed');
+	var instances_gallery = M.Materialbox.init(gallery);
+});
